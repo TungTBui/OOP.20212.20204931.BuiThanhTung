183a3f5016aeb44cb1644c7382d1a24899c7b00e
Bui Thanh Tung <buithanhtung0702@gmail.com>
2022-04-09 12:00:26 +0700

    Add some exercises

---
+++ Lab01/Exercise 6.3, 6.4/src/Ex63.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ex63 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("n = ");
+        int n = scanner.nextInt();
+        for (int i; i = 0, i<n)
+}
