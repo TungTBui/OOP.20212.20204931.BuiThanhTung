2e0518c05d5cccd69bc2c22e7475b43b7b8c4703
TungTBui <89192127+TungTBui@users.noreply.github.com>
2022-05-21 11:44:15 +0700

    Switch to the AIMS project folder
    From now on (Lab 04), we switch to use the AIMS project folder instead of Lab//xx folder

--- AimsProject/src/DigitalVideoDisc.java
+++ AimsProject/src/DigitalVideoDisc.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 
 public class DigitalVideoDisc {
 	
@@ -6,10 +7,18 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	private LocalDate dateAdded;
+	private int id;
+	private static int nbDigitalVideoDiscs = 0;
 	
 	public String getTitle() {
 		return title;
 	}
+	
+	public void setTitle(String newTitle) {
+		title = newTitle;
+	}
+	
 	public String getCategory() {
 		return category;
 	}
@@ -25,12 +34,20 @@ public class DigitalVideoDisc {
 	public DigitalVideoDisc(String title) {
 		super();
 		this.title = title;
+		dateAdded = LocalDate.now();
+		nbDigitalVideoDiscs ++;
+		id = nbDigitalVideoDiscs;
 	}
+
+
 	public DigitalVideoDisc(String title, String category, float cost) {
 		super();
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		dateAdded = LocalDate.now();
+		nbDigitalVideoDiscs ++;
+		id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, float cost) {
 		super();
@@ -38,6 +55,9 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		dateAdded = LocalDate.now();
+		nbDigitalVideoDiscs ++;
+		id = nbDigitalVideoDiscs;
 	}
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
 		super();
@@ -46,6 +66,9 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		dateAdded = LocalDate.now();
+		nbDigitalVideoDiscs ++;
+		id = nbDigitalVideoDiscs;
 	}
 	
 }
